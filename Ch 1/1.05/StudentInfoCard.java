0205857d6c0ff802fcd9ba70edac335191771078
Mihir Achyuta <57376418+Mihir-Achyuta@users.noreply.github.com>
2020-06-05 18:24:37 -0700

    Chapter 1 Section

---
+++ Ch 1/1.05/StudentInfoCard.java
@@ -0,0 +1,40 @@
+//Student Name : Mihir Achyuta 
+//Date : 08/17/19
+//Purpose of Program : The purpose of this program is to show information found on a typical student id card. 
+
+
+public class StudentInfoCard {
+public static void main(String[] args) {
+
+System.out.println("**********************************************************************************************************");	
+System.out.println("*    Mihir Achyuta      08/17/19    Student ID Card Info Program                                                                                                    *");
+System.out.println("*                                                                                                        *");
+System.out.println("**********************************************************************************************************");
+
+
+
+System.out.println("Name : Mihir Achyuta         			Birthday : September 10th 2003");
+System.out.println("Age : 15                     			Grade Level: 11th"   );
+System.out.println();
+	
+System.out.print("School : Evergreen Valley High School   	City: San Jose, CA  ");	
+System.out.println();
+
+System.out.print("Cell Phone : 669-271-5750       text or call / available 2pm-12pm PDT");
+System.out.println("Email : mihirachyuta@gmail.com");
+System.out.println();
+
+System.out.println("Mom's Contact Information:");
+System.out.print("Cell-Phone: 408-636-8309         text / available 7pm-11pm PDT");
+System.out.println("Email : aharipriya@gmail.com");
+System.out.println();
+
+System.out.println("Current Math Class: AP Calculus BC");
+System.out.println("Programming Experience : I know a good chunk of Python and went to 1-2 hackathons to see how they are." + "\r\n" + "I also am part of the programming team at my school's robotics club.");
+System.out.println();
+
+System.out.println("I am taking APCS because I need myself to forcibly to learn how to code so I can apply it for other"+  "\r\n" + "extracurriculars like USACO and hackathons" );
+System.out.println("My school won't let me take APCS so I have to take it here. Also I feel like learning more advanced" + "\r\n" +"Python will be easier if I further solidify my Python foundations in Java. ");
+
+}
+}
